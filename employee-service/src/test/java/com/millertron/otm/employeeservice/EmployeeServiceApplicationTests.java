aaebe3a7b7a80d4aa8811624030c72ad1e8febb9
millertron <senjinkoha@gmail.com>
2020-05-18 19:12:22 +0100

    Added employee-service

---
+++ employee-service/src/test/java/com/millertron/otm/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millertron.otm.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
