e55132844bc7fb0c12ade3d09d129c67b612df13
millertron <senjinkoha@gmail.com>
2020-05-19 17:36:32 +0100

    configured task service with mysql/flyway and added task table

---
+++ task-service/src/main/java/com/millertronics/otm/taskservice/models/Task.java
@@ -0,0 +1,24 @@
+package com.millertronics.otm.taskservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+
+    private Long employeeId;
+}
---
+++ task-service/src/main/resources/db/migration/V0_0_1__initial-migration.sql
@@ -0,0 +1,1 @@
+create table task (id int primary key auto_increment, name varchar(255), description text, employee_id int);
