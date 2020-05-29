638cc166c5f851fc6c3098408148c433987d94e3
millertron <senjinkoha@gmail.com>
2020-05-29 13:55:30 +0100

    Configured security on employee service pointing to auth service

--- employee-service/src/main/java/com/millertronics/otm/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/millertronics/otm/employeeservice/EmployeeServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableResourceServer
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
