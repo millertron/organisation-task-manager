42558fb888c16b00c204f58c000cc7758dd04530
millertron <senjinkoha@gmail.com>
2020-06-01 16:44:07 +0100

    Configured web security

---
+++ web-app/src/main/java/com/millertronics/otm/webapp/config/WebSecurity.java
@@ -0,0 +1,29 @@
+package com.millertronics.otm.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@Order(1000)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+}
